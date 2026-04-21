1299404c9a251a719ce2b846f1ec333060b73b24
isa0-gh <0isa@proton.me>
2026-04-21 20:41:35 +0300

    feat: add JwtID to User model for revocation

--- internal/model/user.go
+++ internal/model/user.go
@@ -10,6 +10,7 @@ type User struct {
 	ID        uint           `gorm:"primaryKey" json:"id"`
 	Email     string         `gorm:"uniqueIndex;not null" json:"email"`
 	Name      string         `json:"name"`
+	JwtID     string         `gorm:"type:uuid" json:"-"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
