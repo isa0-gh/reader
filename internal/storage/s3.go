75744ce7aa2d4bd48f48c30c2c65ed057d14f885
isa0-gh <0isa@proton.me>
2026-04-23 22:38:23 +0300

    feat(storage): add S3Client with PresignPut and PublicURL

---
+++ internal/storage/s3.go
@@ -0,0 +1,64 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/isa0-gh/reader/internal/config"
+)
+
+type S3Client struct {
+	client    *s3.Client
+	presigner *s3.PresignClient
+	bucket    string
+	cdnPrefix string
+}
+
+func NewS3Client(cfg *config.Config) *S3Client {
+	awsCfg := aws.Config{
+		Region:      cfg.S3.Region,
+		Credentials: credentials.NewStaticCredentialsProvider(cfg.S3.AccessKeyID, cfg.S3.SecretAccessKey, ""),
+	}
+
+	opts := []func(*s3.Options){
+		func(o *s3.Options) { o.UsePathStyle = cfg.S3.UsePathStyle },
+	}
+	if cfg.S3.Endpoint != "" {
+		opts = append(opts, func(o *s3.Options) {
+			o.BaseEndpoint = aws.String(cfg.S3.Endpoint)
+		})
+	}
+
+	client := s3.NewFromConfig(awsCfg, opts...)
+	return &S3Client{
+		client:    client,
+		presigner: s3.NewPresignClient(client),
+		bucket:    cfg.S3.Bucket,
+		cdnPrefix: cfg.CDN,
+	}
+}
+
+// PresignPut returns a presigned PUT URL for direct browser upload.
+func (s *S3Client) PresignPut(ctx context.Context, key, contentType string) (string, error) {
+	req, err := s.presigner.PresignPutObject(ctx, &s3.PutObjectInput{
+		Bucket:      aws.String(s.bucket),
+		Key:         aws.String(key),
+		ContentType: aws.String(contentType),
+	}, s3.WithPresignExpires(15*time.Minute))
+	if err != nil {
+		return "", fmt.Errorf("presign: %w", err)
+	}
+	return req.URL, nil
+}
+
+// PublicURL returns the CDN or S3 URL for a key.
+func (s *S3Client) PublicURL(key string) string {
+	if s.cdnPrefix != "" {
+		return s.cdnPrefix + "/" + key
+	}
+	return fmt.Sprintf("https://%s.s3.amazonaws.com/%s", s.bucket, key)
+}
