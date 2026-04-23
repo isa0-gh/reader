c98eb9d6af45e9dbf5e474a2a7a1cc778baedb08
isa0-gh <0isa@proton.me>
2026-04-23 23:06:03 +0300

    feat(service): add ListSeries to SeriesService

--- internal/service/series_service.go
+++ internal/service/series_service.go
@@ -10,6 +10,7 @@ import (
 type SeriesService interface {
 	GetSeries(ctx context.Context, id uint) (*model.Series, error)
 	CreateSeries(ctx context.Context, s *model.Series) (*model.Series, error)
+	ListSeries(ctx context.Context) ([]model.Series, error)
 }
 
 type seriesService struct{ repo repository.SeriesRepository }
@@ -27,4 +28,8 @@ func (s *seriesService) CreateSeries(ctx context.Context, series *model.Series) 
 		return nil, err
 	}
 	return series, nil
+}
+
+func (s *seriesService) ListSeries(ctx context.Context) ([]model.Series, error) {
+	return s.repo.List(ctx)
 }
