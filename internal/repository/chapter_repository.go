9eced8fddda71eb7711bc66508629704e59ab3f9
isa0-gh <0isa@proton.me>
2026-04-23 21:54:30 +0300

    feat(repository): add ChapterRepository with GetByID and eager-loaded pages

---
+++ internal/repository/chapter_repository.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/isa0-gh/reader/internal/model"
+	"gorm.io/gorm"
+)
+
+type ChapterRepository interface {
+	GetByID(ctx context.Context, id uint) (*model.Chapter, error)
+}
+
+type chapterRepository struct{ db *gorm.DB }
+
+func NewChapterRepository(db *gorm.DB) ChapterRepository {
+	return &chapterRepository{db: db}
+}
+
+func (r *chapterRepository) GetByID(ctx context.Context, id uint) (*model.Chapter, error) {
+	var c model.Chapter
+	if err := r.db.WithContext(ctx).Preload("Pages").First(&c, id).Error; err != nil {
+		return nil, err
+	}
+	return &c, nil
+}
