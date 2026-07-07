d6f8e6acb74b125ec64ed2a5b7a72bef7a259a66
isa0-gh <0isa@proton.me>
2026-07-07 22:09:58 +0300

    feat(model): add Comment model
    Comments attach to chapters; author preloaded via User relation for display.

---
+++ internal/model/comment.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Comment struct {
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	ChapterID uint           `gorm:"not null;index" json:"chapter_id"`
+	UserID    uint           `gorm:"not null;index" json:"user_id"`
+	User      *User          `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE" json:"user,omitempty"`
+	Body      string         `gorm:"type:text;not null" json:"body"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
